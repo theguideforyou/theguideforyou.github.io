519558d199c1db2208e3b12f9b1153776858a4b7
HolyxDev <gustavorodriguesofficial@gmail.com>
2024-01-02 20:48:03 -0300

    Adicionado a linguagem 'kotlin'

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,9 +10,8 @@ export function Card({ title, iconPath, detailBgColor, link }: Props) {
     <a href={link} className='langCardAnchor'>
       <div className='langCardBox flex-1 bg-zinc-800 text-white rounded-2xl p-4 flex border border-red-500 items-center gap-4 cursor-pointer hover:bg-transparent transition-all'>
         <div className={`${detailBgColor} rounded-xl p-2 flex items-center justify-center`}>
-          <img src={iconPath} alt={title} className="rounded-xl w-10 h-10" />
+          <img src={iconPath} alt={title} className='rounded-lg w-10 h-10' />
         </div>
-
         <span className='font-medium text-lg'>{title}</span>
       </div>
     </a>
