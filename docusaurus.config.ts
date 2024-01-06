95310069606c538349878a1e07a7c016cc905e02
ROBERT II <h4rt3ck@pm.me>
2024-01-06 23:51:12 +0000

    feat: new favicon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 const config: Config = {
   title: 'TheGuideForYou',
   tagline: 'Prático, didático e multi-linguagem',
-  favicon: 'static/img/readme/TheGuideLogo.png',
+  favicon: 'static/img/TheGuideLogo.ico',
 
   // Set the production url of your site here
   url: 'https://theguideforyou.github.io/',
