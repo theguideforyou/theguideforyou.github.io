148d6ed9bc5c7b2e241d3c63e3eb45884149640a
HolyxDev <gustavorodriguesofficial@gmail.com>
2024-01-10 14:56:31 -0300

    docs: updated languages cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,23 @@
 interface Props {
   title: string;
   iconPath: string;
-  detailBgColor: string;
+  library: string;
+  bgColor: string;
+  textColor: string;
   link: string;
 }
 
-export function Card({ title, iconPath, detailBgColor, link }: Props) {
+export function Card({ title, iconPath, library, bgColor, textColor, link }: Props) {
   return (
     <a href={link} className='langCardAnchor'>
-      <div className='langCardBox flex-1 bg-zinc-800 text-white rounded-2xl p-4 flex border border-red-500 items-center gap-4 cursor-pointer hover:bg-transparent transition-all'>
-        <div className={`${detailBgColor} rounded-xl p-2 flex items-center justify-center`}>
-          <img src={iconPath} alt={title} className='rounded-lg w-10 h-10' />
+      <div className='langCardBox flex-1 bg-[#1d1d1d] text-white rounded-2xl p-4 flex items-center gap-4 cursor-pointer hover:bg-transparent transition-all'>
+        <div className={`${bgColor} rounded-xl p-2 flex items-center justify-center`}>
+          <img src={iconPath} alt={title} className='rounded-lg w-10 h-10 min-w-10 min-h-10' />
         </div>
-        <span className='font-medium text-lg'>{title}</span>
+        <div className='flex flex-col items-start justify-center h-full mb-2 truncate'>
+          <span className='font-medium text-lg text-ellipsis'>{title}</span>
+          <span className={`text-sm ${textColor}`}>{library}</span>
+        </div>
       </div>
     </a>
   )
--- src/components/CardSection.tsx
+++ src/components/CardSection.tsx
@@ -1,6 +1,6 @@
 export function CardSection({ children }: { children: React.ReactNode }) {
   return (
-    <div className='grid grid-cols-3 gap-4 py-8'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 py-8'>
       {children}
     </div>
   )
