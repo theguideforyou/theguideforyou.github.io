eed0d6fd28c8cbd8d3840f0fb6a2584a8025fe59
Gustavo Rodrigues <60802544+hlxdev@users.noreply.github.com>
2024-01-10 19:30:58 -0300

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\peric\\OneDrive\\Área de Trabalho\\doc-teste-ts - Copia\\website\\node_modules\\.pnpm\\infima@0.2.0-alpha.43\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\peric\\OneDrive\\Área de Trabalho\\doc-teste-ts - Copia\\website\\node_modules\\.pnpm\\@docusaurus+theme-classic@3.0.1_@types+react@18.2.45_react-dom@18.2.0_react@18.2.0_typescript@5.2.2\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\peric\\OneDrive\\Área de Trabalho\\doc-teste-ts - Copia\\website\\node_modules\\.pnpm\\@docusaurus+theme-classic@3.0.1_@types+react@18.2.45_react-dom@18.2.0_react@18.2.0_typescript@5.2.2\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\peric\\OneDrive\\Área de Trabalho\\doc-teste-ts - Copia\\website\\src\\css\\custom.css"),
-];
